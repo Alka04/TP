10abe6965e30c7c66d56bedc86c37991e5579e48
Alka04 <alka04006@utb.vaxjo.se>
2022-11-24 12:29:09 +0100

    TP

---
+++ TP.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+    int resultat;
+    if (!int.TryParse(Console.ReadLine(), out resultat))
+        Console.WriteLine("Skriv in en siffra");
+
+    static bool TryParse(string s, out int result)
+    {
+        if ()
+        {
+            
+        }
+    }
