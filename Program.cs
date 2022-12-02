8977b7315280e6cfcd4bf623f8132260a5d422a9
Alka04 <alka04006@utb.vaxjo.se>
2022-12-02 21:16:28 +0100

    Tror jag är klar

--- Program.cs
+++ Program.cs
@@ -4,15 +4,30 @@ using System.Linq;
 using System.Threading.Tasks;
 class Program
 {
-    static void Main()
+    static void Main(string[] args)
     {
-        int resultat;
-        if (!int.TryParse(Console.ReadLine(), out resultat))
-            Console.WriteLine("Skriv in en siffra");
+        bool bFortsätt = true;
+
+        while (bFortsätt)
+        {
+            Console.WriteLine("Skriv ett nummer: ");
+            string numInput = Console.ReadLine();
+            
+            int num = 0;
+            bool success = Int32.TryParse(numInput, out num);
+
+            if (success)
+            {
+                bFortsätt = false;
+                Console.WriteLine(num);
+            }
+            else
+            {
+                Console.WriteLine("Felaktigt värde");
+            }
+        }
+        Console.WriteLine("Klicka för att avsluta");
+
+        Console.ReadLine();
     }
-    public static bool TryParse(string s, out int result)
-    {
-        return int.TryParse(s, out result);
-    }
-    
 }
