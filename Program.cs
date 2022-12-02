dcbd568320b0d6357764383cec7baddf0f72cab2
Alka04 <alka04006@utb.vaxjo.se>
2022-12-02 18:54:31 +0100

    inlämnad

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+class Program
+{
+    static void Main()
+    {
+        int resultat;
+        if (!int.TryParse(Console.ReadLine(), out resultat))
+            Console.WriteLine("Skriv in en siffra");
+    }
+    public static bool TryParse(string s, out int result)
+    {
+        return int.TryParse(s, out result);
+    }
+    
+}
